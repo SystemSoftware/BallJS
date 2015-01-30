5c5257585d6502271c808f4a930cf318067813d6
Manuel Eiden <eidenm@uni-trier.de>
2015-01-30 14:28:43 +0100

    Client: Exceptions and Logging.

--- client.php
+++ client.php
@@ -10,40 +10,37 @@ define('WSDL_URL', $argv[0]);
 define('NODEJS_SERVICE_URL', $argv[1]);
 
 $soapclient = new SoapClient(WSDL_URL);
-
 $curl_handler = curl_init(NODEJS_SERVICE_URL);
 curl_setopt($curl_handler, CURLOPT_POST, 1);
 
 do {
-    $soapball = $soapclient->getBall();
-    $ball = convertBall($soapball);
-    sleep($ball->{'hold-time'});
-    $updatedBall = updateBall($ball);
+    try{
+        $soapball = $soapclient->getBall();
+        $ball = convertBall($soapball);
+        
+        $time = time();
+        $ballId = $ball->id;
+        $lastHere = $time;
 
-    $post = array();
-    $post['ball'] = json_encode($updatedBall);
-    curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $post);
+        if (isset($ball->payload->{'cmr-php-soap-client'} )){
+            $lastHere = $ball->payload->{'cmr-php-soap-client'};
+        }
+        $roundTripTime = $time - $lastHere;
 
-    $response = curl_exec($curl_handler);
+        print "Received ball $ballId; Roundtrip-time: $roundTripTime ms";
+        sleep($ball->{'hold-time'});
+        $updatedBall = updateBall($ball);
 
+        $post = array();
+        $post['ball'] = json_encode($updatedBall);
+        curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $post);
+
+        $response = curl_exec($curl_handler);
+    }
+    catch(Exception $e){
+        print "No Ball available: $e";
+    }
     usleep(DELAY);
 } while(true);
 
-// immer wieder soap-server pollen
-// wenn  ein ball zurueck kommt
-//    hop erhoehen
-//    rundenlaufzeit berechnen, ausgeben
-//    dann auf rest-server pushen
-
-// alternative mit mehreren baellen: 
-// soap-server nach baellen fragen
-// wenn  etwas da ist
-//    für jeden ball
-//       hop erhoehen
-//       rundenlaufzeit berechnen, ausgeben
-//       dann auf rest-server pushen
-
-
-// @todo was ist wenn mehrere baelle existieren? sehen wir das ueberhaupt?
-// 
 ?>
