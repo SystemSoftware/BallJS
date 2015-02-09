13e906ce8d0a618aa150e1fab8d35770b1b564d4
Richard Kiefer <s4rikief@uni-trier.de>
2015-02-09 12:42:10 +0100

    Added usage description. Fixed wrong command-line argument handling.

--- client.php
+++ client.php
@@ -2,12 +2,16 @@
 
 require('Ball.php');
 
+if (count($argv) != 3) {
+    printf("Usage: php %s wsdl-url rest-url\n", $argv[0]);
+    exit(1);
+}
 
 /** delay between server-pollings in microseconds
  */
 define('DELAY', 1* 1000 * 1000);
-define('WSDL_URL', $argv[0]);
-define('NODEJS_SERVICE_URL', $argv[1]);
+define('WSDL_URL', $argv[1]);
+define('NODEJS_SERVICE_URL', $argv[2]);
 
 $soapclient = new SoapClient(WSDL_URL);
 $curl_handler = curl_init(NODEJS_SERVICE_URL);
