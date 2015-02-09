aca6ff3eb541b02643f55915d416d049473c4421
Richard Kiefer <s4rikief@uni-trier.de>
2015-02-09 14:12:37 +0100

    Added verbose option for showing or hiding details about soap faults.

--- client.php
+++ client.php
@@ -2,8 +2,11 @@
 
 require('Ball.php');
 
-if (count($argv) != 3) {
-    printf("Usage: php %s wsdl-url rest-url\n", $argv[0]);
+if (count($argv) < 3) {
+    printf("Usage: php %s wsdl-url rest-url verbose?\n", $argv[0]);
+    print ("\n");
+    print ("If third argument is given, details about soap faults\n");
+    print ("will be printed.\n");
     exit(1);
 }
 
@@ -12,6 +15,12 @@ if (count($argv) != 3) {
 define('DELAY', 1* 1000 * 1000);
 define('WSDL_URL', $argv[1]);
 define('NODEJS_SERVICE_URL', $argv[2]);
+if (count($argv) >= 4) {
+    define('VERBOSE', true);
+}
+else {
+    define('VERBOSE', false);
+}
 
 $soapclient = new SoapClient(WSDL_URL);
 $curl_handler = curl_init(NODEJS_SERVICE_URL);
@@ -21,7 +30,9 @@ do {
     try{
         $soapresponse = $soapclient->getBall();
 
-        $ball = new Ball($soapresponse);        
+        $ball = new Ball($soapresponse);
+
+        print "\n";        
         $ball->greet();
         sleep($ball->getHoldTime());
         $ball->update();
@@ -31,9 +42,20 @@ do {
         curl_exec($curl_handler);
     }
     catch(SoapFault $sf){
-        print "Soap Fault: ". $sf->faultstring ."\n";
+        print_soap_fault($sf);
     }
     usleep(DELAY);
 } while(true);
 
+
+
+
+function print_soap_fault($sf) {
+    if (VERBOSE) {
+        print "Soap Fault: ". $sf->faultstring ."\n";
+    }
+    else {
+        print ".";
+    }
+}
 ?>
